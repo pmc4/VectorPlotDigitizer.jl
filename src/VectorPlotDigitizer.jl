f685271c57ac2933776403d67cb675cc40db09d2
pmc4 <117096890+pmc4@users.noreply.github.com>
2023-09-08 18:01:22 +0200

    Add hello world for testing purposes.
    Because
    * I want to see if the structure of the package works.

--- src/VectorPlotDigitizer.jl
+++ src/VectorPlotDigitizer.jl
@@ -1,5 +1,15 @@
 module VectorPlotDigitizer
 
 # Write your package code here.
+export hellow_orld
+
+"""
+    hellow_orld()
+
+This function is for testing purposes. I want to see if the package structure works.
+"""
+function hellow_orld()
+    print("Hellow, olrd!")
+end
 
 end
