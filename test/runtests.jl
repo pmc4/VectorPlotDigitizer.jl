43e73b58132612ba97b864f463c0c3a1ebe1d42d
pmc4 <117096890+pmc4@users.noreply.github.com>
2023-09-08 17:03:56 +0200

    Add Aqua.jl kwargs options

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,7 +4,7 @@ using Aqua
 
 @testset "VectorPlotDigitizer.jl" begin
     @testset "Code quality (Aqua.jl)" begin
-        Aqua.test_all(VectorPlotDigitizer)
+        Aqua.test_all(VectorPlotDigitizer; ambiguities = false, unbound_args = false)
     end
     # Write your tests here.
 end
