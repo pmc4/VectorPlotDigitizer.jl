ddf3789a4526cf7c4961af2ac316483ff645bd70
Documenter.jl <documenter@juliadocs.github.io>
2023-12-13 13:32:05 +0000

    build based on 8e55010

---
+++ v0.1.0-alpha/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = VectorPlotDigitizer","category":"page"},{"location":"#VectorPlotDigitizer","page":"Home","title":"VectorPlotDigitizer","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for VectorPlotDigitizer.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [VectorPlotDigitizer]","category":"page"}]
+}
