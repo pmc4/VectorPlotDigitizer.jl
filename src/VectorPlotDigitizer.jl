fda6633a9b01513e6cfb9d5d9e1a63f62a533b33
pmc4 <117096890+pmc4@users.noreply.github.com>
2023-12-18 21:48:54 +0100

    Export fill_zeros_bins function.

--- src/VectorPlotDigitizer.jl
+++ src/VectorPlotDigitizer.jl
@@ -4,7 +4,7 @@ using DelimitedFiles
 
 export calibrate_axis, extract_svgpath
 
-export binedges_to_bin_midpoints
+export binedges_to_bin_midpoints, fill_zeros_bins
 
 include("core_functions.jl")
 include("helper_functions.jl")
